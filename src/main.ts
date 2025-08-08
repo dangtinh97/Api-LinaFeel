46234def734c2b1239144b0c41e9e8a84bf99202
tinhvd <dangtinha2@gmail.com>
2025-08-08 17:58:55 +0700

    add response

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfig } from './app.config';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['debug', 'log', 'error', 'warn'],
@@ -10,8 +13,10 @@ async function bootstrap() {
   const logger = new Logger();
   const configService = app.get(ConfigService);
   const port = configService.get<AppConfig['APP_PORT']>('APP_PORT')!;
+  app.useGlobalInterceptors(new ResponseInterceptor(new Reflector()));
   app.enableCors();
-
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(port);
   logger.log('App running port: ' + port);
 }
