02b7e8d6b2c5fe1e16c328900f003abfb8f27b8b
dangtinhvu <dangtinhhaui@gmail.com>
2025-10-17 21:45:53 +0700

    add time

--- src/modules/gemini/gemini.service.ts
+++ src/modules/gemini/gemini.service.ts
@@ -78,7 +78,7 @@ export class GeminiService {
       'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent';
     contents = this.mapContent(contents);
     const time = dayjs(new Date())
-      .add(7, 'minutes')
+      .add(7, 'hours')
       .format('hh:mm DD/MM/YYYY')
       .toString();
     const prompt = `
