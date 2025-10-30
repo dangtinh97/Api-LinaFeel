4bf65992bae71932e3d1d8cfe94b050d9d1aa115
tinhvd <dangtinha2@gmail.com>
2025-10-30 08:49:52 +0700

    update app

--- src/common/index.ts
+++ src/common/index.ts
@@ -48,7 +48,7 @@ export const uuidv4 = (): string => {
 
 export const payloadFromToken = (jwtToken: string) => {
   try {
-    const parts = jwtToken.replace('Bearer ','').split('.');
+    const parts = jwtToken.replace('Bearer ', '').split('.');
     const encodedPayload = parts[1];
 
     // Decode the Base64Url encoded payload
@@ -59,4 +59,117 @@ export const payloadFromToken = (jwtToken: string) => {
   } catch (e) {
     return {};
   }
+};
+
+export const normalizeText = (input: string): string => {
+  return input
+    .replace(/[\x00-\x1F\x7F]+/g, (m) => (m.length ? '. ' : ''))
+    .replace(/\.{2,}/g, '.')
+    .replace(/[\u200B-\u200D\uFEFF\u00A0]/g, ' ')
+    .replace(/\s+/g, ' ') // Xóa các khoảng trắng liền nhau
+    .replace(/^\. +|\. +$/g, '')
+    .trim(); // Xóa khoảng trắng ở đầu và cuối chuỗi
+};
+
+export const _tokenize = (text: string) => {
+  text = text.replace(/(\d)[.,](?=\d)/g, (match, p1) => {
+    return match.includes('.') ? `${p1}_dot_` : `${p1}_comma_`;
+  });
+  text = text.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '__dash__');
+  const sp = text.split('');
+  if (!'.,!?。！？、'.includes(sp[sp.length - 1])) {
+    text = `${text}.`;
+  }
+
+  const segments = [];
+  const regex = /([^.,!?。！？、]+)([.,!?。！？、]+)/g;
+  let match;
+  while ((match = regex.exec(text)) !== null) {
+    const str = match[1].trim();
+    if (/^[.,!?。！？、]+$/.test(str) === false) {
+      const text = str
+        .replaceAll('\\', '')
+        .replaceAll('_comma_', ',')
+        .replaceAll('_dot_', '.')
+        .replaceAll('__dash__', '-');
+      segments.push({
+        text,
+        punctuation: match[2].trim(),
+      });
+    }
+  }
+  return splitAtNearestWord(mergeText(segments, 200), 200);
+};
+function splitAtNearestWord(data: string[], maxLength = 200) {
+  const result = [];
+
+  data.forEach((item) => {
+    if (item.length > maxLength) {
+      let splits = splitText(item, maxLength, []);
+      splits.forEach((resultSplit) => {
+        result.push(resultSplit);
+      });
+    } else {
+      result.push(item);
+    }
+  });
+
+  return result;
+}
+
+function splitText(item, maxLength, result = []) {
+  if (item.length <= maxLength) {
+    if (item.trim()) result.push(item.trim());
+    return result;
+  }
+
+  const text = item;
+  const splitIndex = text.lastIndexOf(' ', maxLength);
+
+  if (splitIndex !== -1) {
+    const part1 = text.slice(0, splitIndex).trim();
+    const part2 = text.slice(splitIndex).trim();
+    if (part1) result.push(part1);
+    return splitText(part2, maxLength, result);
+  } else {
+    const part1 = text.slice(0, maxLength).trim();
+    const part2 = text.slice(maxLength).trim();
+    if (part1) result.push(part1);
+    return splitText(part2, maxLength, result);
+  }
+}
+const mergeText = (arr: any, maxLength: number) => {
+  const result = [];
+  let tempText = arr[0].text + arr[0].punctuation;
+  let tempPunctuation = arr[0].punctuation;
+
+  for (let i = 1; i < arr.length; i++) {
+    // If the combined length does not exceed maxLength
+    if (
+      tempText.length + arr[i].text.length + arr[i].punctuation.length <=
+      maxLength
+    ) {
+      tempText += ' ' + arr[i].text + arr[i].punctuation;
+      tempPunctuation = arr[i].punctuation; // Update punctuation
+    } else {
+      // If punctuation is a period, push the current combined text and start a new one
+      result.push(tempText.trim());
+      tempText = arr[i].text + arr[i].punctuation;
+      tempPunctuation = arr[i].punctuation;
+    }
+
+    // If the punctuation is a period, push the current text and stop merging further
+    if (['.', '!', '?'].indexOf(tempPunctuation) !== -1) {
+      result.push(tempText.trim());
+      tempText = '';
+      tempPunctuation = '';
+    }
+  }
+
+  // Add the last part if it's not empty
+  if (tempText) {
+    result.push(tempText.trim());
+  }
+
+  return result.map((item) => normalizeText(item));
 };
--- src/modules/edge-tts/edge-tts.controller.ts
+++ src/modules/edge-tts/edge-tts.controller.ts
@@ -1,26 +1,16 @@
 import { Controller, Post, Req, Res } from '@nestjs/common';
-import {
-  UniversalEdgeTTS,
-  UniversalCommunicate,
-  UniversalVoicesManager,
-  listVoicesUniversal,
-} from 'edge-tts-universal';
+import { UniversalEdgeTTS } from 'edge-tts-universal';
+import { _tokenize, normalizeText } from '../../common';
+import * as _ from 'lodash';
 @Controller('tts')
 export class EdgeTtsController {
-  @Post('/edge')
+  @Post('/google')
   async createAudio(@Req() req: any, @Res() res: any) {
     //https://learn.microsoft.com/en-us/azure/ai-services/speech-service/language-support?tabs=tts#standard-voices
     const { text, language } = req.body;
-    const voices = {
-      'vi-VN': 'vi-VN-NamMinhNeural',
-      'en-US': 'en-US-EmmaMultilingualNeural'
-    }
-    const tts = new UniversalEdgeTTS(
-      text,
-      voices[language] ?? voices['vi-VN'],
-    );
-    const result = await tts.synthesize();
-    const audioBuffer = Buffer.from(await result.audio.arrayBuffer());
+    const [langCode, langCountry] = language.split('-');
+    console.log(langCountry);
+    const audioBuffer = await this.gtts(text, langCode);
     res.set({
       'Content-Type': 'audio/mpeg',
       'Content-Disposition': 'inline; filename="speech.mp3"',
@@ -28,5 +18,48 @@ export class EdgeTtsController {
     });
 
     res.send(audioBuffer);
+  }
+
+  async edgeTts(text: string, language: string) {
+    const voices = {
+      'vi-VN': 'vi-VN-NamMinhNeural',
+      'en-US': 'en-US-EmmaMultilingualNeural',
+    };
+    const tts = new UniversalEdgeTTS(text, voices[language] ?? voices['vi-VN']);
+    const result = await tts.synthesize();
+    return Buffer.from(await result.audio.arrayBuffer());
+  }
+
+  async gtts(text: string, language: string) {
+    text = normalizeText(text);
+    const data = text.length < 200 ? [text] : _tokenize(text);
+    const p = await Promise.all(
+      data.map((item) => this.curlToGoogle(item, language)),
+    );
+    return await this.combineBase64Audio(p);
+  }
+
+  async curlToGoogle(text: string, language: string) {
+    try {
+      text = encodeURIComponent(encodeURIComponent(text));
+      const url = `https://www.google.com/async/translate_tts?ttsp=tl:${language},txt:${text},spd:1.5&async=_fmt:jspb`;
+      const curl = await fetch(url);
+      const body = await curl.text();
+      const json = body.replace(")]}'", '');
+      return _.get(JSON.parse(json), 'translate_tts.0');
+    } catch (error) {
+      console.log(text, 'error');
+      return null;
+    }
+  }
+
+  async combineBase64Audio(base64Array: string[]): Promise<Buffer> {
+    // Convert each base64 string to binary and concatenate them
+    const combinedBuffer = base64Array.reduce((acc, base64Data) => {
+      const buffer = Buffer.from(base64Data, 'base64');
+      return Buffer.concat([acc, buffer]);
+    }, Buffer.alloc(0));
+
+    return combinedBuffer;
   }
 }
